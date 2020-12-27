aca8d0f59b7522976fbf54bc2646c3baa40e0844
leonardcser <leonardcser@users.noreply.github.com>
2020-12-27 14:54:04 +0100

    Intial commit

---
+++ src/com/leo/piapplication/Main.java
@@ -0,0 +1,15 @@
+/*
+ *	Author:      Leonard Cseres
+ *	Date:        27.12.20
+ *	Time:        14:48
+ */
+
+
+package com.leo.piapplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
